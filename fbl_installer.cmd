6fc4894006f9c26f4ae42d6bc1388a8361005389
Tingkai Liu <tl2747@columbia.edu>
2020-10-23 14:18:52 -0400

    Update fbl_installer.cmd
    update neuromynerva installation.

--- fbl_installer.cmd
+++ fbl_installer.cmd
@@ -1,17 +1,15 @@
 conda create -n fbl_installation python=3.7 -y
 activate fbl_installation
 conda install nodejs scipy pandas cookiecutter git yarn -c conda-forge -y
-pip install jupyter jupyterlab==2.1.5
+pip install jupyter jupyterlab>=2.2.8
 pip install txaio twisted autobahn crochet service_identity autobahn-sync matplotlib h5py seaborn fastcluster networkx msgpack
 pip install pypiwin32
 mkdir fbl_installation
 cd fbl_installation
 git clone https://github.com/FlyBrainLab/Neuroballad.git
 git clone https://github.com/FlyBrainLab/FBLClient.git
-git clone https://github.com/FlyBrainLab/NeuroMynerva.git
 cd ./Neuroballad
 python setup.py develop
 cd ../FBLClient
 python setup.py develop
-cd ../NeuroMynerva
-jlpm & jlpm run build & jupyter labextension install . & jupyter lab build & jupyter lab
+jupyter labextension install @flybrainlab/neuromynerva
