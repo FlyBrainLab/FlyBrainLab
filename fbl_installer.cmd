d9badf55414ff60c366d300ec0a9e4ddb1046d0a
M. K. Turkcan <mkt2126@columbia.edu>
2020-11-02 13:36:06 -0500

    Update fbl_installer.cmd

--- fbl_installer.cmd
+++ fbl_installer.cmd
@@ -1,5 +1,3 @@
-conda create -n fbl_installation python=3.7 -y
-activate fbl_installation
 conda install nodejs scipy pandas cookiecutter git yarn -c conda-forge -y
 pip install jupyter jupyterlab>=2.2.8
 pip install txaio twisted autobahn crochet service_identity autobahn-sync matplotlib h5py seaborn fastcluster networkx msgpack
