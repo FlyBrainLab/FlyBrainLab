a1201443b5329a4bd6e163a90e171bb0e2c87934
Stuart Berg <bergs@janelia.hhmi.org>
2020-11-04 10:30:23 -0500

    Rewrite mac installer to use mamba, and conda-forge for as many packages as possible.

--- fbl_installer_mac.sh
+++ fbl_installer_mac.sh
@@ -1,12 +1,47 @@
-conda install nodejs scipy pandas cookiecutter git yarn -c conda-forge -y
-pip install jupyter jupyterlab>=2.2.8
-pip install txaio twisted autobahn crochet service_identity autobahn-sync matplotlib h5py seaborn fastcluster networkx msgpack msgpack-numpy
+#!/bin/bash
+
+set -e
+
+eval "$(conda shell.bash hook)"
+conda create -n flybrainlab mamba -c conda-forge -y
+conda activate flybrainlab
+
+conda_pkgs=(
+    'python=3.7'
+    nodejs
+    scipy
+    pandas
+    cookiecutter
+    git
+    yarn
+    txaio
+    twisted
+    autobahn
+    crochet
+    service_identity
+    matplotlib
+    h5py
+    seaborn
+    fastcluster
+    networkx
+    msgpack-python
+    msgpack-numpy
+    jupyter
+    'jupyterlab>=2.2.8'
+)
+
+mamba install ${conda_pkgs[@]} -c conda-forge -y
+pip install autobahn-sync
+jupyter labextension install @flybrainlab/neuromynerva
+
 mkdir FlyBrainLab
 cd FlyBrainLab
+
 git clone https://github.com/FlyBrainLab/Neuroballad.git
 git clone https://github.com/FlyBrainLab/FBLClient.git
+
 cd ./Neuroballad
 python setup.py develop
+
 cd ../FBLClient
 python setup.py develop
-jupyter labextension install @flybrainlab/neuromynerva
