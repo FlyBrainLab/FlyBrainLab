801b00d655bba4605eb55a444b407eca60b96f1d
mkturkcan <mehmetkeremturkcan@gmail.com>
2020-11-17 18:14:42 -0500

    Updates to installation for faster data loading

--- fbl_installer.cmd
+++ fbl_installer.cmd
@@ -1,5 +1,7 @@
 pip install jupyter jupyterlab>=2.2.8 & ^
-pip install txaio twisted autobahn crochet service_identity autobahn-sync matplotlib h5py seaborn fastcluster networkx msgpack msgpack-numpy & ^
+pip install txaio twisted autobahn crochet service_identity matplotlib h5py seaborn fastcluster networkx msgpack msgpack-numpy & ^
+pip install autobahn[twisted]==19.2.1 & ^
+pip install git+https://github.com/mkturkcan/autobahn-sync.git & ^
 pip install pypiwin32 & ^
 mkdir fbl_installation & ^
 cd fbl_installation & ^
