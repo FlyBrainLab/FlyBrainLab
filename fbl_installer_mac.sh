fccd9616fb1d38f784553e775e50ccd804086560
Yiyin Zhou <yiyin@ee.columbia.edu>
2020-12-06 04:06:40 -0500

    more fix on the script

--- fbl_installer_mac.sh
+++ fbl_installer_mac.sh
@@ -1,6 +1,7 @@
 conda install nodejs scipy pandas cookiecutter git yarn -c conda-forge -y
 pip install jupyter "jupyterlab>=2.2.8"
-pip install txaio twisted autobahn crochet service_identity autobahn-sync matplotlib h5py seaborn fastcluster networkx msgpack msgpack-numpy
+pip install txaio autobahn[twisted]==19.2.1 crochet service_identity matplotlib h5py seaborn fastcluster networkx msgpack msgpack-numpy
+pip install git+https://github.com/mkturkcan/autobahn-sync.git
 mkdir FlyBrainLab
 cd FlyBrainLab
 git clone https://github.com/FlyBrainLab/Neuroballad.git
